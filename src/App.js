a12ee826d39ff9cf98aaa468bd827db6ec9b9bc2
mccwong <mauricewong@mac.com>
2022-06-23 20:24:20 +0800

    where

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Test from './Test.js';
+
+
 function App() {
   return (
     <div className="App">
+      <Test />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/Test.js
@@ -0,0 +1,29 @@
+// import React, {Component} from 'react';
+// import ReactDOM from "react-dom"
+
+
+function Test () {
+    // return <div>hello hello</div>
+    return (
+      <div className="App">
+        <div> from App() </div>
+        <div> from Test </div>
+      </div>
+    ) ; 
+
+}
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <div> from App() </div>
+//         <div> from Test </div>
+//       </div>
+//     ) ; 
+//   }  
+  
+// }
+
+export default Test;
